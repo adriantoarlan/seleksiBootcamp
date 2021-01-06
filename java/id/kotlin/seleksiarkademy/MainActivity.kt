1a1a080bb70972f02a6c5989ad2ebb8fe44e1b70
Arlan Adrianto <adriantoarlan@gmail.com>
2021-01-06 22:20:17 +0800

    Add files via upload

---
+++ java/id/kotlin/seleksiarkademy/MainActivity.kt
@@ -0,0 +1,16 @@
+package id.kotlin.seleksiarkademy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+//import com.codingtive.androidtutorial.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
